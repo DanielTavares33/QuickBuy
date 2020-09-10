21effb1df945056b8fb37e44c977153ddf56f6e4
DanielTavares33 <57456238+DanielTavares33@users.noreply.github.com>
2020-09-10 22:22:05 +0100

    Create and configure DbContext and DbSet

---
+++ QuickBuy.Repositorio/Contexto/QuickBuyContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Dominio.Entity;
+using QuickBuy.Dominio.ObjectValue;
+
+namespace QuickBuy.Repositorio.Contexto
+{
+	public class QuickBuyContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+		public DbSet<Request> Requests { get; set; }
+		public DbSet<Product> Products { get; set; }
+		public DbSet<ItemRequest> ItemsRequests { get; set; }
+		public DbSet<PaymentMethod> PaymentMethod { get; set; }
+	}
+}
