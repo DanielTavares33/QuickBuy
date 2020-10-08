c5263909f34849b99a85065b71b266ba4ab79b78
DanielTavares33 <57456238+DanielTavares33@users.noreply.github.com>
2020-10-08 18:59:21 +0100

    Configure the ProductRepository and configure methods in BaseRepository

--- QuickBuy.Repositorio/Repositories/ProductRepository.cs
+++ QuickBuy.Repositorio/Repositories/ProductRepository.cs
@@ -1,13 +1,17 @@
 ﻿using QuickBuy.Dominio.Contracts;
 using QuickBuy.Dominio.Entity;
+using QuickBuy.Repositorio.Contexto;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace QuickBuy.Repositorio.Repositories
 {
 	public class ProductRepository : BaseRepository<Product>, IProductRepository
 	{
-
+		public ProductRepository(QuickBuyContext quickBuyContext) : base(quickBuyContext)
+		{
+		}
 	}
 }
