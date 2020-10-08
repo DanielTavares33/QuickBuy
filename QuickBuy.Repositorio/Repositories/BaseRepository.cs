4b40fedc5eb32bdd4d8fd87934413d9618637d99
DanielTavares33 <57456238+DanielTavares33@users.noreply.github.com>
2020-10-08 18:44:55 +0100

    Add Context to Repository

--- QuickBuy.Repositorio/Repositories/BaseRepository.cs
+++ QuickBuy.Repositorio/Repositories/BaseRepository.cs
@@ -1,20 +1,29 @@
 ﻿using QuickBuy.Dominio.Contracts;
+using QuickBuy.Repositorio.Contexto;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace QuickBuy.Repositorio.Repositories
 {
 	public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
 	{
+		private readonly QuickBuyContext _quickBuyContext;
+
+		public BaseRepository(QuickBuyContext quickBuyContext)
+		{
+			_quickBuyContext = quickBuyContext;
+		}
+
 		public void Add(TEntity entity)
 		{
-			throw new NotImplementedException();
+			_quickBuyContext.Set<TEntity>().Add(entity);
 		}
 
 		public IEnumerable<TEntity> GetAll()
 		{
-			throw new NotImplementedException();
+			return _quickBuyContext.Set<TEntity>().ToList();
 		}
 
 		public TEntity GetById(int id)
