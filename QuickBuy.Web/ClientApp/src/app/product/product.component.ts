ccbd8ccec36c7f4f53f0e7d18ad29d936380f775
DanielTavares33 <57456238+DanielTavares33@users.noreply.github.com>
2020-10-15 23:17:55 +0100

    Creating Product component typescript

---
+++ QuickBuy.Web/ClientApp/src/app/product/product.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "produto", //tag onde o ProductComponent vai ser renderizado
+    template: ""
+})
+export class ProductComponent {
+    public name: string;
+
+}
