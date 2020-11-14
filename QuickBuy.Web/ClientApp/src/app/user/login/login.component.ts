e2a170f77ddf4a278aad584629ab4a78ca9d0a7e
DanielTavares33 <57456238+DanielTavares33@users.noreply.github.com>
2020-11-14 18:25:18 +0000

    Correct redirecting at authentication

--- QuickBuy.Web/ClientApp/src/app/user/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/user/login/login.component.ts
@@ -1,6 +1,6 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import { User } from "src/app/model/user";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
     selector: "app-login", 
@@ -8,18 +8,23 @@ import {Router} from "@angular/router";
     styleUrls: ["./login.component.css"]
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit{
     public user;
     public userAuthentication: boolean;
+    public returnUrl: string;
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private activatedRouter: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
         this.user = new User();
     }
-    
+
     login(){
         if (this.user.email == "dantavper@hotmail.com" && this.user.password == "123") {
             sessionStorage.setItem("user-authenticated","1");
-            this.router.navigate(['/'])
+            this.router.navigate([this.returnUrl])
         }
 
     }
