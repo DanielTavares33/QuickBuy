3472ad4698dcdf27abbc4eb3eb977003466644c0
DanielTavares33 <57456238+DanielTavares33@users.noreply.github.com>
2020-11-15 16:56:13 +0000

    Creation of UserController

--- QuickBuy.Web/ClientApp/src/app/user/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/user/login/login.component.ts
@@ -1,19 +1,19 @@
 import {Component, OnInit} from "@angular/core";
 import { User } from "src/app/model/user";
 import {ActivatedRoute, Router} from "@angular/router";
+import { UserService } from "src/app/services/user/user.service";
 
 @Component({
     selector: "app-login", 
     templateUrl: "./login.component.html",
     styleUrls: ["./login.component.css"]
 })
-
 export class LoginComponent implements OnInit{
     public user;
     public userAuthentication: boolean;
     public returnUrl: string;
 
-    constructor(private router: Router, private activatedRouter: ActivatedRoute) {
+    constructor(private router: Router, private activatedRouter: ActivatedRoute, private userService: UserService) {
     }
 
     ngOnInit(): void {
@@ -22,10 +22,13 @@ export class LoginComponent implements OnInit{
     }
 
     login(){
-        if (this.user.email == "dantavper@hotmail.com" && this.user.password == "123") {
-            sessionStorage.setItem("user-authenticated","1");
-            this.router.navigate([this.returnUrl])
-        }
+
+        this.userService.verifyUser(this.user).subscribe();
+
+        // if (this.user.email == "dantavper@hotmail.com" && this.user.password == "123") {
+        //     sessionStorage.setItem("user-authenticated","1");
+        //     this.router.navigate([this.returnUrl])
+        // }
 
     }
         
---
+++ QuickBuy.Web/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Dominio.Entity;
+using System;
+
+namespace QuickBuy.Web.Controllers
+{
+	public class UserController : Controller
+	{
+		[HttpPost]
+		public ActionResult Post()
+		{
+			try
+			{
+
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.ToString());
+			}
+		}
+
+		[HttpGet]
+		public ActionResult Get()
+		{
+			try
+			{
+
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.ToString());
+			}
+		}
+
+		[HttpPost("VerifyUser")]
+		public ActionResult VerifyUser([FromBody] User user)
+		{
+			try
+			{
+				if (user.Email == "danpertav@gmail.com" && user.Password == "123")
+				{
+					return Ok(user);
+				}
+				return BadRequest("User or password is not valid");
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.ToString());
+			}
+		}
+	}
+}
