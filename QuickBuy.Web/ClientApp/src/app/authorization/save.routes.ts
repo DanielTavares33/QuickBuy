e5c024b164affd67ce79a07d76a4e78aa33da55c
DanielTavares33 <57456238+DanielTavares33@users.noreply.github.com>
2020-11-16 20:12:55 +0000

    Inject UserService at SaveRoutes and at Nav component

--- QuickBuy.Web/ClientApp/src/app/authorization/save.routes.ts
+++ QuickBuy.Web/ClientApp/src/app/authorization/save.routes.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
+import { UserService } from "../services/user/user.service";
 
 @Injectable({
     providedIn: 'root'
@@ -8,10 +9,11 @@ import { Observable } from "rxjs";
 export class SaveRoutes implements CanActivate{
     
 
-    constructor(private router: Router){
+    constructor(private router: Router, private userService: UserService){
     }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+        // this.userService
         var authenticated = sessionStorage.getItem("user-authenticated")
         if (authenticated == "1") {
             return true;
